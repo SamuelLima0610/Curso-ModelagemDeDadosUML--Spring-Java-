ccfbaebcd5f71152b666cb3907f723792ad21d90
Samuel Vitorio Lima <34277130+SamuelLima0610@users.noreply.github.com>
2020-11-04 16:28:58 -0300

    Testing the first domain class - Category

--- src/main/java/com/samuel/cursonc/resources/CategoryResources.java
+++ src/main/java/com/samuel/cursonc/resources/CategoryResources.java
@@ -1,14 +1,24 @@
 package com.samuel.cursonc.resources;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.samuel.cursonc.domain.Category;
 
 @RestController
 @RequestMapping(value="/category")
 public class CategoryResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "Rest está funcionando";
+	public List<Category> list() {
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		List<Category> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
